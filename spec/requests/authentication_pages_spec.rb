473a14aba94dbfcd863cbaade63e436deba8662f
Unai <unai.martinez@bidenet.com>
2014-03-12 11:23:43 +0100

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -89,7 +89,29 @@ describe "Authentication" do
         end
       end
       
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+      
       describe "in the Users controller" do
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
 
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
@@ -107,6 +129,7 @@ describe "Authentication" do
         end
       end
     end
+    
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
@@ -124,5 +147,4 @@ describe "Authentication" do
       end
     end
   end
-  
 end
